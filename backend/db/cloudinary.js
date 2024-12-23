9e15549328ca6b5fb2ab22649bc229d9e0db3b11
Fernando Goncalves <fernando_goncalves_santos@hotmail.com.br>
2024-12-23 13:58:08 -0300

    config das variaveis de ambiente cloudinary

--- backend/db/cloudinary.js
+++ backend/db/cloudinary.js
@@ -2,7 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 
 cloudinary.config({
-  secure: true, 
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_SECRECT 
 });
 
 module.exports = cloudinary;
